26782adbe3517af6e66608d178a118f307cb5454
moyo diallo <moy3ib@gmail.com>
2017-11-30 23:17:33 +0100

    change les bouton de niveau en liste

--- GameView.java
+++ GameView.java
@@ -1,3 +1,5 @@
+import java.utils.*;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -7,17 +9,12 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
+
 public class GameView extends Application {
 
 	private GameBoard gameModel;
 
-	private Button btnNiveau1;
-	private Button btnNiveau2;
-	private Button btnNiveau3;
-	private Button btnNiveau4;
-	private Button btnNiveau5;
-	private Button btnNiveau6;
-	private Button btnNiveau7;	
+	private ArrayList<Button> btnNiveauList;	
 
 	public GameView(){		
 	}
